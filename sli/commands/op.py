d922703c737af43a20c1179e03e215e7e45223d7
Andrew Mallory <amallory4668@gmail.com>
2021-04-22 16:04:13 -0400

    op command

--- sli/commands/op.py
+++ sli/commands/op.py
@@ -7,44 +7,45 @@ from jinja2 import Template
 import json
 
 skillet_template = """
-name: sli_capture
+name: sli_op
 type: pan_validation
 snippets:
 
-  - name: capture_test
-    cmd: parse
-    variable: config
+  - name: op_test
+    cmd: op
+    cmd_str: {{ cmd_str }}
+    output_type: xml
     outputs:
-      - name: capture_test
+      - name: op_output
         capture_{{ capture_method }}: {{ capture_arg }}
 """
 
-valid_methods = ['list', 'object', 'expression']
+valid_methods = ['list', 'object']
 
 def print_usage():
-  print('Usage for capture module:\n  sli capture [method] [query] [context-variable:optional]\n')
+  print('Usage for capture module:\n  sli op [cmd] [method] [query] [context-variable:optional]\n')
   print('valid Methods:\n   ' + '\n   '.join(valid_methods))
 
+class OpCommand(BaseCommand):
 
-class Capture(BaseCommand):
-
-    sli_command = 'capture'
-    short_desc = 'Capture a value based on object, list, or expression'
+    sli_command = 'op'
+    short_desc = 'Run an operational command'
     no_skillet = True
 
     @require_ngfw_connection_params
     def run(self):
 
         # Render validation skillet for execution
-        if len(self.args) < 2 or len(self.args) > 3:
+        if len(self.args) < 3 or len(self.args) > 4:
           print_usage()
           return
 
-        capture_method = self.args[0]
-        capture_arg = self.args[1]
+        cmd_str = self.args[0]
+        capture_method = self.args[1]
+        capture_arg = self.args[2]
         capture_var = ''
-        if len(self.args) == 3:
-          capture_var = self.args[2]
+        if len(self.args) == 4:
+          capture_var = self.args[3]
         if not capture_method in valid_methods:
           print(f'Invalid method - {capture_method}')
           print_usage()
@@ -52,7 +53,8 @@ class Capture(BaseCommand):
 
         skillet_yaml = Template(skillet_template).render({
           'capture_method': capture_method,
-          'capture_arg': capture_arg
+          'capture_arg': capture_arg,
+          'cmd_str': cmd_str
         })
         skillet_dict = yaml.safe_load(skillet_yaml)
         sl = SkilletLoader()
@@ -65,7 +67,7 @@ class Capture(BaseCommand):
           return
 
         # Print captured JSON 
-        output = exe['outputs']['capture_test']
+        output = exe['outputs']['op_output']
         print(json.dumps(output, indent=4))
 
         # Update context if using context
