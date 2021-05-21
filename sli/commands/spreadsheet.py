89c13dbda565914876e237dfbe36580c1fa9db10
Andrew Mallory <amallory4668@gmail.com>
2021-05-21 13:25:23 -0400

    spreadsheet command

--- sli/commands/spreadsheet.py
+++ sli/commands/spreadsheet.py
@@ -2,6 +2,7 @@ from sli.decorators import require_single_skillet
 from sli.decorators import require_skillet_type
 from .base import BaseCommand
 
+from jinja2 import Environment, meta
 import xlsxwriter
 import os
 
@@ -75,6 +76,49 @@ class SpreadsheetCommand(BaseCommand):
                 variable_list.append(variable['name'])
                 row += 1
             row = 1
+
+            # Add set commands to workbook
+            for line in contents:
+
+                # Read the line and create a variable set
+                env = Environment()
+                var_set = sorted(meta.find_undeclared_variables(env.parse(line)))
+
+                # Check if line has a single variable
+                if len(var_set) == 1:
+                    # Replace quote with 2xquotes to be excel friendly
+                    line = line.replace('"', '""').strip()
+                    var_name = var_set[0]
+                    jinja_var = "{{ " + var_name + " }}"
+                    cell_pos = variable_list.index(var_name)
+
+                    # Formula format  = =SUBSTITUTE("set deviceconfig system dns-setting servers primary {0}", "{0}", '0-Config Values'!B21)
+                    form_line = f'=SUBSTITUTE("{line}", "{jinja_var}", \'values\'!B{cell_pos})'
+                    worksheet_set.write(row, 0, form_line)
+
+                # Check if line has 2 variables when requires nested SUBSTITUTEs
+                elif len(var_set) == 2:
+                    # Replace quote with 2xquotes to be excel friendly
+                    line = line.replace('"', '""').strip()
+                    var_name_0 = var_set[0]
+                    jinja_var_0 = "{{ " + var_name_0 + " }}"
+                    cell_pos_0 = variable_list.index(var_name_0)
+
+                    var_name_1 = var_set[1]
+                    jinja_var_1 = "{{ " + var_name_1 + " }}"
+                    cell_pos_1 = variable_list.index(var_name_1)
+
+                    # Inner is first substitute and all is nested substitute for excel
+                    form_line_inner = f'SUBSTITUTE("{line}", "{jinja_var_0}", \'values\'!B{cell_pos_0})'
+                    form_line_all = f'=SUBSTITUTE({form_line_inner}, "{jinja_var_1}", \'values\'!B{cell_pos_1})'
+                    worksheet_set.write(row, 0, form_line_all)
+
+                else:
+                    # Replace quotes and also remove hidden quotes for standard non-formula cells
+                    line = line.replace('"', '""').strip()
+                    worksheet_set.write(row, 0, line)
+
+                row += 1
 
             workbook.close()
             print(f"Workbook completed and saved to {out_file}")
