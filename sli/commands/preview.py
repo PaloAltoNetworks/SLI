3ee5b6cb3ad214c488a8b6f3aa92c7376f62f549
Nathan <nembery@gmail.com>
2021-07-13 16:28:30 -0400

    allow pan-validation in preview

--- sli/commands/preview.py
+++ sli/commands/preview.py
@@ -25,7 +25,7 @@ class PreviewCommand(BaseCommand):
 """
 
     @require_single_skillet
-    @require_skillet_type("panos", "panorama")
+    @require_skillet_type("panos", "panorama", "pan-validation")
     @load_variables
     @require_config
     def run(self, config):
