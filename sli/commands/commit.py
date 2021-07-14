a769c4e1d9e46d571d6f5370c5608a1642a7b5f9
Nathan <nembery@gmail.com>
2021-07-14 13:44:25 -0400

    add commit command

---
+++ sli/commands/commit.py
@@ -0,0 +1,24 @@
+from skilletlib import Panoply
+from skilletlib.exceptions import PanoplyException
+
+from .base import BaseCommand
+from ..decorators import require_ngfw_connection_params
+from ..decorators import require_panoply_connection
+
+
+class CommitCommand(BaseCommand):
+    sli_command = "commit"
+    short_desc = "Commit the Candidate configuration"
+    no_skillet = True
+
+    @require_ngfw_connection_params
+    @require_panoply_connection
+    def run(self, pan: Panoply):
+        try:
+            print("Committing configuration...")
+            self.sli.skillet.panoply.commit()
+            print("Success...")
+
+        except PanoplyException as pe:
+            print(pe)
+            exit(1)
