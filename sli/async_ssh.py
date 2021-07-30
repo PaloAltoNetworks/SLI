4539f08c8a2c2c398d98f261b1aa454608d0efcd
Andrew Mallory <amallory4668@gmail.com>
2021-07-30 14:34:09 -0400

    command script

--- sli/async_ssh.py
+++ sli/async_ssh.py
@@ -16,6 +16,7 @@ class AsyncSSHSession:
         self.c_stderr = None
         self.prompt = None
         self.hostname = None
+        self.has_error = False
 
     async def connect(self):
         self.client = await asyncssh.connect(
@@ -59,18 +60,80 @@ class AsyncSSHSession:
             return re.match('^.*@.*[#>]$', test_data[-1].strip())
         return None
 
-    async def cli_command(self, command):
+    def error_check_output(self, output):
         """
-        Sends a command and returns response as string
+        Return True on detecting error in output
         """
-        self.c_stdin.write(command + "\n")
-        data = ''
+        lines = output.split("\n")
+        for line in lines:
+            if line.startswith("Unknown command:"):
+                self.has_error = True
+
+    async def recv_until_prompt(self, echo=False):
+        """
+        Receive data from stdin until a prompt is seen, return any text
+        """
         prompt_found = None
+        data = ''
         while not prompt_found:
             recv_data = await self.c_stdout.read(self.BUFFER_LEN)
             recv_data = recv_data.replace("\r\n", "\n")
             data += recv_data
             prompt_found = self._check_for_prompt(recv_data)
 
-        # Return all lines after the first line as that will contain the submitted command
-        return "\n".join(data.split("\n")[1:])
+        # Return all lines except the first and last line
+        if echo:
+            return data
+        return "\n".join(data.split("\n")[1:-1])
+
+    async def cli_command(self, command, echo=False):
+        """
+        Sends a command and returns response as string
+        """
+        self.has_error = False
+        self.c_stdin.write(command + "\n")
+        output = await self.recv_until_prompt(echo=echo)
+        self.error_check_output(output)
+        return output
+
+    async def run_command_script(self, lines, out_file=None):
+        """
+        Take a list of lines and run them as a script, return True if no error
+        Return False if error
+        """
+        self.has_error = False
+        ll = lines
+        if isinstance(ll, str):
+            ll = ll.split("\n")
+
+        # Setup output options
+        session_output = None
+        echo = False
+        if out_file:
+            session_output = open(out_file, "w")
+            echo = True
+
+        for line in ll:
+            if not len(line):
+                continue
+            output = await self.cli_command(line.strip(), echo=echo)
+
+            # Save output to file if specified
+            if out_file:
+                session_output.write(output)
+            else:
+                session_output += output
+
+            # Stop running the script if an error was found
+            self.error_check_output(output)
+            if self.has_error:
+                if out_file:
+                    session_output.close()
+                    return False
+                return session_output
+
+        # Only return output lines if not saving to a file
+        if out_file:
+            session_output.close()
+            return True
+        return session_output
